147d2e424c8de3d203064f4e76c11eeb0624f731
Davi Campos Gonçalves <davi19_19@hotmail.com>
2017-10-25 16:48:24 -0200

    Iniciado formulário de dados pessoais.

--- PortalSicoobDivicred/Controllers/PrincipalController.cs
+++ PortalSicoobDivicred/Controllers/PrincipalController.cs
@@ -50,13 +50,13 @@ namespace PortalSicoobDivicred.Controllers
                 var Cookie = Request.Cookies.Get("CookieFarm");
 
                 var Login = Criptografa.Descriptografar(Cookie.Value);
-               /* if (VerificaDados.PrimeiroLogin(Login))
+                if (VerificaDados.PrimeiroLogin(Login))
                 {
-                    //   return View("FormularioCadastro");
-                    return View();
+                    return View("FormularioCadastro");
+
                 }
                 else
-                {*/
+                {
                     var DadosUsuarioBanco = VerificaDados.RecuperaDadosUsuarios(Login);
 
                     if (VerificaDados.PermissaoCurriculos(DadosUsuarioBanco[0]["login"]))
@@ -75,7 +75,8 @@ namespace PortalSicoobDivicred.Controllers
                     TempData["ImagemPerfil"] = "https://docs.google.com/uc?id=0B2CLuTO3N2_obWdkajEzTmpGeU0";
                     return View();
                 }
-            
+            }
+
             return RedirectToAction("Login", "Login");
             }
         

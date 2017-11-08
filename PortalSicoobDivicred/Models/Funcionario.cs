34830ad86d4345593a89fce4d91bbf13e42c84d5
Davi Campos Gonçalves <davi.campos@divicred.com.br>
2017-11-08 11:20:28 -0200

    Adcionado Model Funcionario e mask js
    - Adcionado model de funcionarios
    - Adcionado plugin de mascara no formulario funcionario

---
+++ PortalSicoobDivicred/Models/Funcionario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalSicoobDivicred.Models
+{
+    public class Funcionario
+    {
+
+        public string NomeFuncionario { get; set; }
+
+        public string CpfFuncionario { get; set; }
+
+        public string RgFuncionario { get; set; }
+
+        public string PisFuncionario { get; set; }
+
+        public string DataNascimentoFuncionario { get; set; }
+
+        public string FormacaoAcademica { get; set; }
+
+        public string UsuarioSistema { get; set; }
+
+        public string Email { get; set; }
+
+        public string PA { get; set; }
+
+        public string Rua { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Funcao { get; set; }
+
+        public string QuatidadeFilho { get; set; }
+
+        public string DataNascimentoFilho { get; set; }
+
+        public string ContatoEmergencia { get; set; }
+
+        public string PrincipaisHobbies { get; set; }
+
+        public string ComidaFavorita { get; set; }
+
+        public string Viagem { get; set; }
+
+        
+    }
+}

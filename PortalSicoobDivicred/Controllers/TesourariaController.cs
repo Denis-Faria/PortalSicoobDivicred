dd67d43395e1666303008c6f5da189ae39e80d77
Denis Faria <denis.tavares@divicred.com.br>
2018-02-01 17:04:43 -0200

    Criação apenas da view e inicio da criação da acction

---
+++ PortalSicoobDivicred/Controllers/TesourariaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortalSicoobDivicred.Controllers
+{
+    public class TesourariaController : Controller
+    {
+        // GET: Tesouraria
+        public ActionResult Tesouraria()
+        {
+            return View("Tesouraria");
+        }
+    }
+}

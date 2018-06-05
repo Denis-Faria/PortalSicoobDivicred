d75bac3ea71a65c992f31bd16352113edec18f66
Davi Campos Gonçalves <davi19_19@hotmail.com>
2018-06-05 17:01:25 -0300

    Ajuste de css, inicio alerts
    -Ajustado css no layou
    - Inicio de laertas para ações

---
+++ PortalSicoobDivicred/Aplicacao/EnviodeAlertas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalSicoobDivicred.Aplicacao
+{
+    public class EnviodeAlertas
+    {
+
+        public void EnviaEmail()
+        {
+
+        }
+
+        public void CadastraAlerta()
+        {
+
+        }
+    }
+}
